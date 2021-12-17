e1e29342782731f20985ccb292c1d24799dd8278
yuriidiiego <yuriidiiego@gmail.com>
2021-12-17 14:51:29 -0300

    Atualização do PSJava

--- src/main/java/br/com/supera/game/store/model/ProductModel.java
+++ src/main/java/br/com/supera/game/store/model/ProductModel.java
@@ -1,58 +1,20 @@
 package br.com.supera.game.store.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 
+@Schema(description = "Product")
+@Getter
+@Setter
 public class ProductModel {
   private int id;
 
-  @Schema(description = "Nome do produto")
   private String name;
 
-  @Schema(description = "Preço do produto")
   private double price;
 
-  @Schema(description = "Pontuação do produto")
-  private short score;
+  private int score;
 
   private String image;
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public double getPrice() {
-    return price;
-  }
-
-  public void setPrice(double d) {
-    this.price = d;
-  }
-
-  public short getScore() {
-    return score;
-  }
-
-  public void setScore(int i) {
-    this.score = (short) i;
-  }
-
-  public String getImage() {
-    return image;
-  }
-
-  public void setImage(String image) {
-    this.image = image;
-  }
 }
