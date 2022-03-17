d0906675643be5ad698bdfb964a30574a1efd49e
Yuri <yuriidiiego@gmail.com>
2022-03-17 19:31:15 -0300

    Adicionando o Schema no model

--- src/main/java/br/com/supera/game/store/model/ProductModel.java
+++ src/main/java/br/com/supera/game/store/model/ProductModel.java
@@ -1,20 +1,25 @@
 package br.com.supera.game.store.model;
 
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-@Schema(description = "Product")
+
 @Getter
 @Setter
 public class ProductModel {
   private int id;
 
+  @Schema(description = "Nome do produto")
   private String name;
 
+  @Schema(description = "Preço do produto")
   private double price;
 
+  @Schema(description = "Pontuação do produto")
   private int score;
 
+  @Schema(description = "Imagem do produto")
   private String image;
 }
